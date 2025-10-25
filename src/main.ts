24ed311212932f6a09bbc7e98bea66efd767054f
David Ferlay <dferlay@skilld.cloud>
2025-10-25 16:12:48 +0200

    Gitignore and test usage of env var

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,11 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// Check access of environment variable
+// Variables must be prefixed with VITE_ to be available to the frontend
+const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+console.log("API Key:", apiKey);
+
+
+
